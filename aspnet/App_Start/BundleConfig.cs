2639558b4bfd09c6d946e93dadecd9805ec4e0e8
iNetherr <xxADVENTUSxx@gmail.com>
2020-11-26 20:07:38 +0200

    Calling an API functions using jQuery + bootbox plug-in

--- aspnet/App_Start/BundleConfig.cs
+++ aspnet/App_Start/BundleConfig.cs
@@ -20,11 +20,12 @@ namespace aspnet
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootbox.js",
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css")); 
         }
     }
 }
